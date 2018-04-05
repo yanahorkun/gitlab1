2eb06ecaf42fd6f1e42faf4c034a525abc23564e
Yana Horkun <yana.sonya.h@gmail.com>
2018-04-05 12:24:44 +0300

    lab2_commit

--- src/gitlab1/Gitlab1.java
+++ src/gitlab1/Gitlab1.java
@@ -21,4 +21,19 @@ public class Gitlab1 {
                      System.out.println("GIT ignore");
     }
     
+    public class SimpleCalc {
+
+private double a;
+private double b;
+
+SimpleCalc(double p1,double p2) {
+this.a=p1;
+this.b=p2;
+}
+
+public double getSum() {
+return this.a+this.b;
+}
+
+}
 }
